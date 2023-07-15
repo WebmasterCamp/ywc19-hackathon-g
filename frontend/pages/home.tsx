6efcce41c4cd5c56e20c009710d33faddcd6c9bd
TumKtp <tumktp.dev@gmail.com>
2023-07-15 17:45:56 +0700

    Add icon

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -10,35 +10,35 @@ import {
 } from '@/constant/enum'
 import { useState } from 'react'
 
-const steps = [
-  {
-    title: 'เงินเดือนที่ต้องการ',
-    desc: 'กรุณาระบุเงินเดือนที่ต้องการ',
-    number: 1,
-  },
-  {
-    title: 'ประเภทของงาน',
-    desc: 'กรุณาระบุประเภทของงานที่ต้องการ \nและเหมาะสมตามประสบการณ์ของคุณ',
-    number: 2,
-  },
-  {
-    title: 'ประสบการณ์',
-    desc: 'คุณมีประสบการณ์ในการทำงานกี่ปี?',
-    number: 3,
-  },
-  {
-    title: 'ประเภทการจ้างงาน',
-    desc: 'คุณต้องการทำงานแบบใด?',
-    number: 4,
-  },
-]
-
 enum FormSteps {
   SALARY = 1,
   JOB = 2,
   EXPERIENCE = 3,
   CONTRACT_TYPE = 4,
 }
+
+const steps = [
+  {
+    title: 'เงินเดือนที่ต้องการ',
+    desc: 'กรุณาระบุเงินเดือนที่ต้องการ',
+    number: FormSteps.SALARY,
+  },
+  {
+    title: 'ประเภทของงาน',
+    desc: 'กรุณาระบุประเภทของงานที่ต้องการ \nและเหมาะสมตามประสบการณ์ของคุณ',
+    number: FormSteps.JOB,
+  },
+  {
+    title: 'ประสบการณ์',
+    desc: 'คุณมีประสบการณ์ในการทำงานกี่ปี?',
+    number: FormSteps.EXPERIENCE,
+  },
+  {
+    title: 'ประเภทการจ้างงาน',
+    desc: 'คุณต้องการทำงานแบบใด?',
+    number: FormSteps.CONTRACT_TYPE,
+  },
+]
 
 export default function Home() {
   const [currectForm, setCurrectForm] = useState(FormSteps.SALARY)
@@ -62,7 +62,12 @@ export default function Home() {
         <div className="w-1/2 flex flex-col mt-5  gap-10">
           {steps.map((step) => (
             <>
-              <Step title={step.title} desc={step.desc} number={step.number} />
+              <Step
+                title={step.title}
+                desc={step.desc}
+                number={step.number}
+                currentForm={currectForm}
+              />
               {/* <div className="h-[50px] border-l-[3px] ml-[30px] border-primary "></div> */}
             </>
           ))}
@@ -103,16 +108,16 @@ export default function Home() {
           <div className="flex gap-5">
             {currectForm !== FormSteps.SALARY && (
               <button
-                className=" bg-transparent w-full h-[48px] rounded-xs text-primary border border-primary mt-5 text-base"
+                className=" bg-transparent w-full h-[48px] rounded-xs text-primary border border-primary mt-5 text-lg font-normal"
                 onClick={() => {
                   setCurrectForm(currectForm - 1)
                 }}
               >
-                Previous step
+                ก่อนหน้า
               </button>
             )}
             <button
-              className="bg-primary w-full h-[48px] rounded-xs text-white border mt-5"
+              className="bg-primary w-full h-[48px] rounded-xs text-white border mt-5 text-lg "
               onClick={() => {
                 if (currectForm !== FormSteps.CONTRACT_TYPE) {
                   setCurrectForm(currectForm + 1)
@@ -126,7 +131,9 @@ export default function Home() {
                 })
               }}
             >
-              {currectForm !== FormSteps.CONTRACT_TYPE ? 'Next' : 'Submit'}
+              <p className="font-normal">
+                {currectForm !== FormSteps.CONTRACT_TYPE ? 'ต่อไป' : 'ส่ง'}
+              </p>
             </button>
           </div>
         </div>
@@ -138,17 +145,33 @@ function Step({
   title,
   desc,
   number,
+  currentForm,
 }: {
   title: string
   desc: string
   number: number
+  currentForm: number
 }) {
   const formattedDesc = desc.replace(/\n/g, '<br>')
 
   return (
     <div className="flex items-center gap-10">
-      <div className="w-[66px] h-[66px] bg-primary rounded-[100%] flex items-center justify-center text-white text-[36px] font-medium">
-        {number}
+      <div
+        className={
+          `w-[66px] h-[66px]  rounded-[100%] flex items-center justify-center text-[36px] font-medium ` +
+          (number <= currentForm
+            ? 'bg-primary text-white'
+            : 'bg-transparent border-2 border-primary text-primary')
+        }
+      >
+        {number >= currentForm ? (
+          number
+        ) : (
+          <img
+            src="/material-symbols-check.svg"
+            className="w-[42px] h-[42px]"
+          ></img>
+        )}
       </div>
       <div className="text-start">
         <div className="text-[28px] font-medium">{title}</div>
