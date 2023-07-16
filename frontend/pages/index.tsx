35c8752671918ce6080b7f6ca4785e498faf80a5
TumKtp <tumktp.dev@gmail.com>
2023-07-16 09:18:18 +0700

    Fix bug

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,27 @@
 import { CourseCard } from '@/components/CourseCard'
 import TextInput from '@/components/TextInput'
 import { courses } from '@/constant/courses'
-import { getUserDetail } from '@/helper/user-helper'
+import { getLatestCourse, getUserDetail } from '@/helper/user-helper'
 
 export default function Main() {
+  const course: any = {
+    id: '1',
+    name: 'ต่อเติม ซ่อมแซมบ้าน',
+    description: 'เรียนรู้งานการก่อโครงหลังคาเหล็ก ต่อเติมบ้าน',
+    location: 'กรุงเทพมหานคร',
+    schedule: [
+      {
+        lessonNo: 1,
+        title: '',
+        dateTime: '2023-07-06 06:31:34',
+      },
+    ],
+    duration: '5 อาทิตย์ (30 ชั่วโมง)',
+    certificate: true,
+    test: true,
+    img: '/images/course-1.png',
+  }
+
   return (
     <div>
       <div className="max-w-[1200px] mx-auto">
@@ -22,21 +40,42 @@ export default function Main() {
           </div>
           <div className="text-2xl font-medium">คอร์สที่คุณกำลังเรียนอยู่</div>
         </div>
-        <img
-          src="/images/banner.svg"
-          className="mt-4 w-full object-contain"
-        ></img>
-        <div className="h-[187px] bg-gray-300 rounded-xs flex items-center px-10 py-5">
-          <div className="text-white">
-            <div>คอร์สเรียนของคุณ</div>
-            <div className="text-4xl">ต่อเติม ซ่อมแซมบ้าน</div>
+        {!course ? (
+          <img
+            src="/images/banner.svg"
+            className="mt-4 w-full object-contain"
+          ></img>
+        ) : (
+          <div className="h-[187px]  rounded-xs flex items-center px-10 py-5 justify-between bg-[url('/banner-bg.svg')] bg-cover mt-4">
+            <div className="flex items-center">
+              <div className="text-white">
+                <div>คอร์สเรียนของคุณ</div>
+                <div className="text-4xl">{course?.name}</div>
+              </div>
+              <div className="w-[1px] h-[120px] border-[0.5px] border-white mx-7"></div>
+              {!course?.complted ? (
+                <div className="text-white">
+                  <div>เรียนไปแล้ว</div>
+                  <div className="flex items-center gap-4">
+                    <div className="text-4xl">
+                      0 ชั่วโมง <span className="text-lg">(0%)</span>
+                    </div>
+                    <div className="bg-gray-200 px-4 py-1 text-xs rounded-[4px]">
+                      เหลืออีก 30 ชั่วโมง
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="text-white text-4xl max-w-[390px]">
+                  ยินดีด้วย คุณเรียนคอร์ส รับเหมาก่อสร้างจบแล้ว !
+                </div>
+              )}
+            </div>
+            <div className="text-lg rounded-xs px-10 py-3 text-white border-white border cursor-pointer">
+              คลิกเพื่อดูรายละเอียด
+            </div>
           </div>
-          <div className="w-[1px] h-[120px] border-[0.5px] border-white mx-7"></div>
-          <div className="text-white">
-            <div>เรียนไปแล้ว</div>
-            <div className="text-4xl">0 ชั่วโมง (0%)</div>
-          </div>
-        </div>
+        )}
         <div className="flex items-center gap-2 mt-6">
           <div className="bg-lightBlue rounded-xs w-[48px] h-[48px] flex items-center justify-center">
             <img src="/learning.svg" className="w-[32px] h-[32px]"></img>
